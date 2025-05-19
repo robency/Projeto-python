6822ddfa066522f2c739994a90d4343eb8483209
robency <robencymichel@gmail.com>
2025-05-19 19:07:23 -0300

    Primeiro commit

---
+++ numerico.py
@@ -0,0 +1,9 @@
+soma = 1+1
+multiplicacao = 4 * 4
+divisao = 30 / 3
+pontencia = 7 ** 2
+
+print("soma", soma)
+print("multiplicacao",multiplicacao)
+print("divisao ", divisao)
+print("divisao ", pontencia)
